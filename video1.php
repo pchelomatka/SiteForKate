e4e4f990b947be33433a8935473d9cd818692530
KirillYaykov <anton97rus@gmail.com>
2018-06-18 13:52:04 +0400

    3 commit

---
+++ video1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>testvideo</title>
+</head>
+<body>
+<iframe src="https://player.vimeo.com/video/257367858" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+</body>
+</html>
